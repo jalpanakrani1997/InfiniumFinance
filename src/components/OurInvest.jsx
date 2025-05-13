ac682a71b9bd01497c7a83a2e53264a55c8ab2ac
jalpanakrani1997 <159773285+jalpanakrani1997@users.noreply.github.com>
2025-05-13 13:53:46 +0530

    Update OurInvest.jsx

--- src/components/OurInvest.jsx
+++ src/components/OurInvest.jsx
@@ -13,7 +13,7 @@ const OurInvest = () => {
         'Quarterly profit distribution'
       ],
       color: 'border-t-[#0c4244]',
-      buttonColor: 'bg-teal-900 hover:bg-[#0c4244]'
+      buttonColor: 'bg-[#093d3e] hover:bg-[#145052]'
     },
     {
       title: 'Tree Family Plan',
@@ -24,8 +24,8 @@ const OurInvest = () => {
         'Collective financial growth',
         'Secure generational wealth'
       ],
-      color: 'border-t-yellow-600',
-      buttonColor: 'bg-yellow-600 hover:bg-yellow-700'
+      color: 'border-t-[#c89b28]',
+      buttonColor: 'bg-[#c89b28] hover:bg-[#b4891d]'
     },
     {
       title: 'Systematic Investment Plan',
@@ -37,15 +37,15 @@ const OurInvest = () => {
         'Long-term wealth creation'
       ],
       color: 'border-t-[#0c4244]',
-      buttonColor: 'bg-teal-900 hover:bg-[#0c4244]'
+      buttonColor: 'bg-[#093d3e] hover:bg-[#145052]'
     }
   ];
 
   return (
-    <section id="how-it-works" className="pattern-dots min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 py-20">
-      <div className="container mx-auto px-4">
+    <section id="how-it-works" className="pattern-dots min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 py-10">
+      <div className="container-fluid   px-4 mr-20 ml-20">
         <div className="text-center mb-12">
-          <h3 className="text-xl md:text-5xl font-bold text-teal-900 mb-4">Our Investment Plans</h3>
+          <h3 className="text-lg md:text-5xl font-bold text-[#0c4244] mb-4">Our Investment Plans</h3>
           <p className="text-gray-600 text-xl max-w-3xl mx-auto">
             Choose from our range of investment options designed to meet your financial goals and secure your future.
           </p>
@@ -59,9 +59,9 @@ const OurInvest = () => {
                 className={`relative group bg-white p-8 rounded-xl shadow-[0_8px_30px_rgb(0,0,0,0.1)] transition-transform duration-500 overflow-hidden flex flex-col justify-between ${step.color} border-t-8`}
               >
                 <div className="flex flex-col gap-4">
-                  <h2 className="text-3xl font-bold text-[#0c4244] mb-2">{step.title}</h2>
-                  <p className='text-gray-500 text-xl'>{step.suggestion}</p>
-                  <ul className=" text-xl space-y-2">
+                  <h2 className="text-2xl font-bold text-[#0c4244] mb-2">{step.title}</h2>
+                  <p className='text-gray-500 text-lg'>{step.suggestion}</p>
+                  <ul className=" text-lg space-y-2">
                     {step.description.map((desc, idx) => (
                       <li key={idx} className="flex items-start gap-2">
                         <LucideCircleCheck className="lucide lucide-circle-check-big h-5 w-5 text-dark-green mr-2 shrink-0 mt-0.5" size={18} />
@@ -81,7 +81,7 @@ const OurInvest = () => {
 
         <div className="mt-12 text-center ">
       
-          <button className="transform transition-all duration-300 ease-in-out hover:shadow-md hover:scale-105 bg-[#0c4244] hover:bg-teal-800 text-white px-8 py-3 rounded-md font-semibold shadow-md flex items-center justify-center gap-2 mx-auto">
+          <button className="transform transition-all duration-300 ease-in-out hover:shadow-md hover:scale-105 bg-[#145052] hover:bg-[#093d3e] text-white px-8 py-3 rounded-md font-semibold shadow-md flex items-center justify-center gap-2 mx-auto">
             View All Investment Plans
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
